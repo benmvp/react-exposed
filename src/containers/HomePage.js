b7f9e56839a9479ba354a7f256eed09f8f1315d9
Ben Ilegbodu <ben@benmvp.com>
2022-01-24 22:05:57 -0800

    Remove emojis for linting

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -4,9 +4,9 @@ export default class HomePage extends Component {
     render() {
         return (
             <div>
-                <h1>React exposed! 😮</h1>
+                <h1>React exposed!</h1>
 
-                <p>Live demos associated with the "React exposed! 😮" talk. Click the links above to navigate different examples.</p>
+                <p>Live demos associated with the "React exposed!" talk. Click the links above to navigate different examples.</p>
 
                 <p><a href="https://github.com/benmvp/react-exposed">View the code on Github</a></p>
             </div>
